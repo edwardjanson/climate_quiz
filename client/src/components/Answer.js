7613d418ddbb0b1349f1a4aea02e3fa391ef7f6a
luisty94 <luistrigoyuste94@gmail.com>
2022-12-19 12:13:34 +0000

    answer component css

--- client/src/components/Answer.js
+++ client/src/components/Answer.js
@@ -3,14 +3,40 @@ import styled from 'styled-components';
 
 
 const Answer = ({onNextQuestion}) => {
+  
   return (
     <Container>
-      <button onClick={onNextQuestion}>Next</button>
+      <AnswerBox>This is the answer</AnswerBox>
+      <StyledButton onClick="">Find Out More</StyledButton>
+      <StyledButton onClick={onNextQuestion}>Next</StyledButton>
     </Container>
   )
 }
 
 const Container = styled.div`
 `
+const StyledButton = styled.button`
+  font-size: inherit;
+  width: 10rem;
+  justify-content: space-between;
+  flex-direction: column;
+  text-align: center;
+  column-gap: 4rem;
+  flex-wrap: wrap;
+  margin: 0.25rem;
+  padding: 0.25rem; 
+`;
+
+const AnswerBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  column-gap: 4rem;
+  flex-wrap: wrap;
+  margin: 0.25rem;
+  padding: 0.25rem; 
+  background: rgb(104 185 255 / 50%);
+  }
+`;
 
 export default Answer;

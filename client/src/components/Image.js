4af2cb8c681f808ae739dba89d78a090976456a1
effrancina <f.v.menichetti@gmail.com>
2022-12-19 11:40:53 +0000

    :hammer: added image question styling

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -5,13 +5,32 @@ import styled from 'styled-components';
 const Image = ({question, onAnswer}) => {
 
   return (
-    <>
+    <Container>
+        <p>Icon</p>
         <p>{question.question}</p>
-        <img onClick={onAnswer} src={question.correct_answer} />
-        <img onClick={onAnswer} src={question.incorrect_answers[0]} />
-    </>
+        <p>Select one</p>
+        <ImageContainer>
+          <ImageItem onClick={onAnswer} src={question.correct_answer} />
+          <ImageItem onClick={onAnswer} src={question.incorrect_answers[0]} />
+        </ImageContainer>
+    </Container>
   );
 }
 
+const Container = styled.span`
+  text-align: center;
 
+`
+const ImageContainer = styled.span`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  margin-bottom: 1rem;
+`
+
+const ImageItem = styled.img`
+  width: 18rem;
+  height: 13rem;
+`
 export default Image;

1b881bd78ca5aac1e5c9c563c8eb0ec35e7610e6
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-12-19 11:53:21 +0000

    :hammer: updated multiple choice and added random answer logic

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -7,17 +7,29 @@ import Image from './Image';
 
 
 const Question = ({question, onAnswer}) => {
+
+  const answers = [question.correct_answer, question.incorrect_answers].flat(1);
+
+  const randomiseQuestions = (answers) => {
+    // using the Fisher Yates Shuffle https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    for (let i = answers.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [answers[i], answers[randomIndex]] = [answers[randomIndex], answers[i]];
+    }
+  }
+
+  randomiseQuestions(answers);
   
   return (
     <>
       {question.question_type === "Multiple Choice" ? 
-            <MultipleChoice question={question} onAnswer={onAnswer} /> : ""}
+            <MultipleChoice questionTitle={question.question} answers={answers} onAnswer={onAnswer} /> : ""}
 
       {question.question_type === "Bool" ? 
-            <Boolean question={question} onAnswer={onAnswer} /> : ""}
+            <Boolean questionTitle={question.question} answers={answers} onAnswer={onAnswer} /> : ""}
 
       {question.question_type === "Images" ? 
-            <Image question={question} onAnswer={onAnswer} /> : ""}
+            <Image questionTitle={question.question} answers={answers} onAnswer={onAnswer} /> : ""}
     </>
   )
 }

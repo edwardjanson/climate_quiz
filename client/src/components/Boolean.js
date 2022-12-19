c506a6e147e3523bf657e73bfb64c27f49569414
Alistair <alistair13_cooke@hotmail.com>
2022-12-19 11:08:49 +0000

    Bool Buttons styled

--- client/src/components/Boolean.js
+++ client/src/components/Boolean.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+// import Button from './Button';
 
 
 const Boolean = ({question, onAnswer}) => {
 
   return (
-    <>
+    <Container>
         <p>{question.question}</p>
-        <button onClick={onAnswer}>{question.correct_answer}</button>
-        <button onClick={onAnswer}>{question.incorrect_answers[0]}</button>
-    </>
+        <ButtonsContainer>
+        <Button onClick={onAnswer}>{question.correct_answer}</Button>
+        <Button onClick={onAnswer}>{question.incorrect_answers[0]}</Button>
+        </ButtonsContainer>
+    </Container>
   );
 }
 
+const Container = styled.div`
+text-align: center;
+`
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`
+
+const Button = styled.button`
+  padding: 1rem;
+  width: 30%;
+`
 
 export default Boolean;
